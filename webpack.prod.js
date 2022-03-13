919d51754c1afdafd81f07dc03429ebcec62dad1
Randy Azman Reyes <imrandyreyes@gmail.com>
2022-03-13 16:51:30 +0800

    Setup production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build/dist'),
+    },
 });
